c2df28c90fe5d1da81f574368c0660d197cb5c27
praka <praka@DESKTOP-4OINM4C>
2023-04-18 10:34:39 -0700

    Run With Testng

--- DSALGO_Main/src/test/java/Testrunner/TestRunner.java
+++ DSALGO_Main/src/test/java/Testrunner/TestRunner.java
@@ -1,12 +1,13 @@
 package Testrunner;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
 		features={"src/test/resources/Feature"},
 		glue ="stepDefinition",
